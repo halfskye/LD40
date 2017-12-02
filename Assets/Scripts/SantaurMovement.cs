1b2be909c8ce2540eeb3473536280adbfd6c4301
Stephen Roper <stephenroper@me.com>
2017-12-01 22:55:10 -0600

    Added basic movement.

--- Assets/Scripts/SantaurMovement.cs
+++ Assets/Scripts/SantaurMovement.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class SantaurMovement : MonoBehaviour {
 
-    public float Gravity;
-    public float Speed;
+    public float gravity;
+    public float speed;
+    public float maxSpeed;
+    public float minSpeed;
+    public float accel;
 
-    private bool _keyLeft() { return (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)); }
-    private bool _keyRight() { return (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)); }
+    private bool _keyLeft() { return (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)); }
+    private bool _keyRight() { return (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)); }
 
     // Use this for initialization
     void Start () {
-		
+        speed = .05f;
+        maxSpeed = .08f;
+        minSpeed = 0.001f;
+        accel = .002f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.right * Speed);
+
+        Movement();
+
     }
+
+    private void Movement()
+    {
+        if (_keyRight() && !_keyLeft())
+        {
+            //speed += accel;
+            transform.Translate(Vector2.right * speed);
+
+            if (speed >= maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+        }
+       
+
+        if (_keyLeft() && !_keyRight())
+        {
+            //speed += accel;
+            transform.Translate(Vector2.left * speed);
+
+            if (speed >= maxSpeed)
+            {
+                speed = maxSpeed;
+            }
+        }
+
+        }
 }
