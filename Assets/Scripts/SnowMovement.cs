3466216e48da8efd56e4164be136133ea7374985
Joshua Watson <josh.m.watson@gmail.com>
2017-12-03 23:49:25 -0600

    added snow spawners

---
+++ Assets/Scripts/SnowMovement.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts;
+using UnityEngine;
+
+public class SnowMovement : MonoBehaviour {
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * SnowRando.Get().GetSnowSpeed() * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "SnowDestroyer")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SnowRando.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SnowRando : MonoBehaviour
+    {
+        //Collect Houses
+        public GameObject[] Snows;
+
+        //Object Pooling
+        private List<GameObject> _snowPool;
+        public int SnowAmountTotal;
+        public int SnowAmountSingle;
+
+        //Time to spawn
+        public float waitForNextMax;
+        public float countDown;
+
+        //X Range
+        public float xMin;
+        public float xMax;
+
+        //Y Range
+        public float yMin;
+        public float yMax;
+
+        [SerializeField]
+        private float _snowSpeed = 3.0f;
+        public float GetSnowSpeed() {
+          return _snowSpeed;
+        }
+
+        static private SnowRando _singleton = null;
+        static public SnowRando Get() {
+          return _singleton;
+        }
+
+        private void Awake() {
+          _singleton = this;
+        }
+
+        void Start()
+        {
+            _snowPool = new List<GameObject>();
+            foreach (var snow in Snows)
+            {
+                for (int j = 0; j < SnowAmountSingle; j++)
+                {
+                    GameObject obj = Instantiate(snow);
+                    obj.SetActive(false);
+                    _snowPool.Add(obj);
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            countDown -= Time.deltaTime;
+            if (countDown <= 0)
+            {
+                SpawnSnow();
+                countDown = Random.Range(1, waitForNextMax);
+            }
+        }
+
+        void SpawnSnow()
+        {
+            if (_snowPool != null)
+            {
+                GameObject gameObj = _snowPool[Random.Range(0, _snowPool.Count)];
+                if (!gameObj.activeInHierarchy)
+                {
+                    gameObj.transform.position = new Vector2(Random.Range(xMin, xMax), Random.Range(yMin, yMax));
+                    gameObj.SetActive(true);
+                }
+            }
+            else
+            {
+                Debug.Log("Add Houses to the HouseRando Script you DumbAss!!!!");
+            }
+        }
+    }
+}
