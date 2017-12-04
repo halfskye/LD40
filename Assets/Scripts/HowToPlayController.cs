2e1050e84c2b072d66a2b137c68327572b63257f
Stephen Roper <stephenroper@me.com>
2017-12-03 21:56:42 -0600

    Updated audio

--- Assets/Scripts/HowToPlayController.cs
+++ Assets/Scripts/HowToPlayController.cs
@@ -37,7 +37,7 @@ public class HowToPlayController : MonoBehaviour {
 
     IEnumerator WaitForTitle()
     {
-        yield return new WaitForSeconds(2.2f);
+        yield return new WaitForSeconds(2.5f);
         _startSelect = true;
 
     }
