452172f3ddb79d909aba0255c301f0e55be1543f
Joshua Watson <josh.m.watson@gmail.com>
2017-12-04 19:05:47 -0600

    Added Spinning Snowflakes

--- Assets/Scripts/SnowMovement.cs
+++ Assets/Scripts/SnowMovement.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class SnowMovement : MonoBehaviour {
 
-    private float _rotationSpeed = 5;
-
     void Update()
     {
-        transform.Translate(Vector2.down * 5 * Time.deltaTime);
+        MoveAndSpin();
+    }
 
-        var angle = (Mathf.Sin(Time.time * _rotationSpeed) + 1.0) / 2.0 * 360;
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.identity, _rotationSpeed * Time.deltaTime);
+    private void MoveAndSpin()
+    {
+        transform.Translate(Vector2.down * 3 * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
